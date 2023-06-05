3e1d74e99f513a2479b82a244224489c24a75fcd
mirakwon24 <118599870+mirakwon24@users.noreply.github.com>
2023-06-05 09:53:19 +0400

    Add files via upload

---
+++ application/db/people.py
@@ -0,0 +1,6 @@
+def get_employees(manager, accountant):
+    staff = manager + accountant
+    print(f"The staff includes {staff} people")
+
+
+get_employees(20, 3)
---
+++ application/salary.py
@@ -0,0 +1,6 @@
+def calculate_salary(wage):
+    salary = wage * 25
+    print(f"Salary is {salary} $")
+
+
+calculate_salary(200)
